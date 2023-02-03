04d6d6c7e7dd1a3a8ea7099ec004b1316e9ffa83
liberal-henrique <luihenlibsou@gmail.com>
2023-02-03 18:44:18 +0000

    starting routes

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Home() {
+  const navigate = useNavigate();
+
+  function navigateHandler() {
+    navigate('products');
+  }
+  return (
+    <>
+      <h1>My Home Page</h1>
+      <p><button onClick={navigateHandler}>Navigate</button></p>
+    </>
+  );
+}
---
+++ src/pages/ProductDetail.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useParams, Link } from 'react-router-dom';
+
+export default function ProductDetailPage() {
+  const params = useParams();
+
+  return (
+    <>
+      <h1>Product Details!</h1>
+      <p>{ params.productId }</p>
+      <p><Link to=".." relative="path">Back</Link></p>
+    </>
+  )
+}
